7434cc424cfb5d5f304560618ef2f52035739d0a
jkl <189156395@qq.com>
2021-02-16 15:37:04 +0800

    微信支付完成

---
+++ api/orders.js
@@ -0,0 +1,41 @@
+import request from "@/utils/request";
+
+export default {
+
+  // 生成订单接口
+  createOrders(courseId) {
+    return request({
+      url: '/eduorder/order/createOrder/' + courseId,
+      method: 'post'
+    })
+  },
+
+
+  // 根据订单id 查询订单接口
+  getOrdersInfo(id) {
+    return request({
+      url: '/eduorder/order/getOrderInfo/'+id,
+      method: 'get'
+    })
+  },
+
+
+  // 生成二维码的方法
+  createNative(orderNo) {
+    return request({
+      url: '/eduorder/paylog/createNative/' + orderNo,
+      method: 'get'
+    })
+  },
+
+
+  // 查询订单状态方法
+  queryPayStatus(orderNo) {
+    return request({
+      url: '/eduorder/paylog/queryPayStatus/' + orderNo,
+      method: 'get'
+    })
+  },
+
+
+}
